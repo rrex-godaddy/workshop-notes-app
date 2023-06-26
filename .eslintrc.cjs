11967b7602dac33282b3af73bd27d16f516e315a
Regina Rex <rrex@godaddy.com>
2023-06-26 11:39:37 -0500

    initial commit: boilerplate

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    'es6': true,
+    'node': true,
+    browser: true, es2020: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    'object-curly-spacing': ['error', 'always'],
+    'semi': 'error',
+    'eol-last': 'error',
+    'allowAllPropertiesOnSameLine': false
+  },
+};
