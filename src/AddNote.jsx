a2c568dbeded9c781aec7eae1bf20051c3c00fa5
Regina Rex <rrex@godaddy.com>
2025-06-25 12:44:29 -0500

    add note

---
+++ src/AddNote.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+function AddNote() {
+  const [textareaValue, setTextareaValue] = useState('')
+
+  return (
+    <form
+      onSubmit={ (event) =>  {
+        event.preventDefault();
+        setTextareaValue('');
+      }}
+    >
+        <label htmlFor="notes-textarea">Enter your notes</label>
+        <textarea
+          id="notes-textarea"
+          name="notes"
+          value={ textareaValue }
+          onChange={
+            (event) => {
+                setTextareaValue(event.target.value);
+            }
+          }
+          placeholder="What's on your mind?"
+        > 
+        </textarea>
+        <button type="submit">
+            Add Note
+        </button>
+    </form>
+  )
+}
+
+export default AddNote
